a199361d9e920146bcfc47cd63d15f143eedb973
Woody Christy <wchristy@cloudera.com>
2013-12-16 11:35:14 -0500

    Added comments to explain ConfigFileGenerator sections

--- src/main/java/com/cloudera/manager/utils/ConfigFileGenerator.java
+++ src/main/java/com/cloudera/manager/utils/ConfigFileGenerator.java
@@ -12,8 +12,10 @@ public class ConfigFileGenerator {
     public static void main(String[] args) throws IOException {
 
         if (args.length == 3) {
+            //If you only want the files and not merged just call this
             HashMap<String, File> bp = CMUtils.getConfigFiles(args[0], args[1], args[2]);
-
+           //For each directory merge all XML files
+           //Ignore this section if you want standard files
             for (Map.Entry<String, File> ks : bp.entrySet()) {
                 System.out.println(ks.getKey() + " merged File exists at: " + Utils.mergeXMLConfigFiles(ks.getValue(), ks.getKey() + ".xml"));
                 FileUtils.deleteDirectory(ks.getValue());
